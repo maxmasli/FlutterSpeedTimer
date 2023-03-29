de8fa525c3b29c1c4a98500711e6d2d41ad723d2
maxmasli <maxspeedcubezzz@gmail.com>
2023-03-29 21:52:23 +0700

    add app bar in settings page

--- lib/features/speedtimer/presentation/pages/settings_page.dart
+++ lib/features/speedtimer/presentation/pages/settings_page.dart
@@ -20,46 +20,99 @@ class SettingsPage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.primary,
         body: BlocBuilder<TimerBloc, TimerState>(
           builder: (context, state) {
-            return ListView(
+            return Column(
               children: [
-                ListTile(
-                  title: Text(
-                    "Press delay",
-                    style: TextStyle(
-                        color: Theme.of(context).textTheme.bodyMedium!.color!),
+                const SettingsAppBar(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      ListTile(
+                        title: Text(
+                          "Press delay",
+                          style: TextStyle(
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .color!),
+                        ),
+                        subtitle: Text(
+                          state.settingsEntity.delay.toString(),
+                          style: TextStyle(
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .color!),
+                        ),
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (_) {
+                              return const PressDelayDialog();
+                            },
+                          );
+                        },
+                      ),
+                      ListTile(
+                        title: Text(
+                          "Change theme",
+                          style: TextStyle(
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .color!),
+                        ),
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (_) {
+                              return const SetThemeDialog();
+                            },
+                          );
+                        },
+                      ),
+                    ],
                   ),
-                  subtitle: Text(
-                    state.settingsEntity.delay.toString(),
-                    style: TextStyle(
-                        color: Theme.of(context).textTheme.bodyMedium!.color!),
-                  ),
-                  onTap: () {
-                    showDialog(
-                      context: context,
-                      builder: (_) {
-                        return const PressDelayDialog();
-                      },
-                    );
-                  },
-                ),
-                ListTile(
-                  title: Text(
-                    "Change theme",
-                    style: TextStyle(
-                        color: Theme.of(context).textTheme.bodyMedium!.color!),
-                  ),
-                  onTap: () {
-                    showDialog(
-                      context: context,
-                      builder: (_) {
-                        return const SetThemeDialog();
-                      },
-                    );
-                  },
                 ),
               ],
             );
           },
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsAppBar extends StatelessWidget {
+  const SettingsAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PhysicalModel(
+      elevation: 5,
+      color: Colors.black,
+      child: Container(
+        height: 60,
+        color: Theme.of(context).colorScheme.secondary,
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () {
+                sl<PageController>().animateToPage(1,
+                    duration: const Duration(milliseconds: 200),
+                    curve: Curves.ease);
+              },
+              icon: Icon(
+                Icons.arrow_forward_ios,
+                color: Theme.of(context).textTheme.bodyMedium!.color,
+              ),
+            ),
+            Text(
+              "Settings",
+              style: TextStyle(
+                  fontSize: 26,
+                  color: Theme.of(context).textTheme.bodyMedium!.color),
+            ),
+          ],
         ),
       ),
     );
